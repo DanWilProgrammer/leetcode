a017dee29187ce4f0f03442204abe78108110fb6
DanWil <141411600+DanWilProgrammer@users.noreply.github.com>
2024-03-30 13:15:07 +0100

    Create 0226-invert-binary-tree.cpp

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+        TreeNode* temp = root->left;
+            
+        root->left = root->right;
+        root->right = temp;
+        
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;      
+    }
+};
